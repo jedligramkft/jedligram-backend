09ab17131908262a62eff40e57b1ce0a0f73cf1a
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2026-02-24 21:13:48 +0100

    Implement PostResource for consistent API responses in PostController and ThreadController; update response formats and remove unnecessary attributes from requests.

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -6,6 +6,7 @@ use App\Models\Thread;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreThreadRequest;
 use App\Http\Requests\CreatePostRequest;
+use App\Http\Resources\PostResource;
 use App\Http\Resources\ThreadResource;
 use Illuminate\Support\Facades\Gate;
 
@@ -70,7 +71,7 @@ class ThreadController extends Controller
         }, function ($query) {
             return $query->latest();
         })->get();
-        return response()->json($posts, 200);
+        return response()->json(PostResource::collection($posts), 200);
     }
 
     /**
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'content' => $this->content,
+            'user_id' => $this->user_id,
+            'thread_id' => $this->thread_id,
+            'score' => $this->score,
+            'age' => $this->created_at->diffForHumans()
+        ];
+    }
+}
