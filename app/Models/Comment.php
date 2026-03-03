6bd3fed0116a090145ab67d65b163ca54a96ffc9
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2026-03-03 12:24:09 +0100

    Add Comment model, factory, and update DatabaseSeeder for comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,13 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Comment extends Model
 {
-    use HasRecursiveRelationships;
+    use HasRecursiveRelationships, HasFactory;
 
     protected $fillable = ['user_id', 'post_id', 'parent_id', 'content'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function parent() {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
 
     public function getParentKeyName()
     {
