b6435e0a0f268b606b8b3c2ec170fae3a430d817
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2026-03-19 10:36:07 +0100

    created test file for posts, added todo to refine testing logic.

---
+++ tests/Feature/PostApiTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Thread;
+use App\Models\ThreadUser;
+use App\Models\User;
+use Database\Seeders\ProductionDataSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->seed(ProductionDataSeeder::class);
+
+    $users = User::factory(20)->create();
+
+    Thread::factory(2)->create()
+        ->each(function ($thread) use ($users) {
+
+            $users->random(rand(3, 10))->each(function ($user) use ($thread) {
+                ThreadUser::create([
+                    'thread_id' => $thread->id,
+                    'user_id' => $user->id,
+                    'role_id' => 3,
+                ]);
+            });
+
+            Post::factory(2)->create([
+                'thread_id' => $thread->id,
+                'user_id' => $users->random()->id,
+            ]);
+        });
+});
+
+//TODO: figure out how to test posts
+
+test('authenticated user can fetch a list of all posts', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user, 'sanctum')
+                     ->getJson('/api/posts');
+
+    $response->assertStatus(200)
+             ->assertJsonCount(4)
+             ->dump();
+});
