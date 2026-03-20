70c735f26fa7bc1a5461d9bde894d3577b254f22
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2026-03-20 10:55:20 +0100

    Add feature tests for post creation and validation rules

--- tests/Feature/PostApiTest.php
+++ tests/Feature/PostApiTest.php
@@ -34,7 +34,7 @@ beforeEach(function () {
 
 //TODO: figure out how to test posts
 describe('Creating a new post inside of a thread', function () {
-    test('authenticated member can create a post inside the thread', function () {
+    test('authenticated member can create a post inside the thread', function (array $validData) {
         $user = User::factory()->create();
         $thread = Thread::factory()->create();
         ThreadUser::create([
@@ -43,10 +43,65 @@ describe('Creating a new post inside of a thread', function () {
             'role_id' => 3,
         ]);
         $response = $this->actingAs($user, 'sanctum')
-            ->getJson('/api/posts');
+            ->postJson("/api/threads/{$thread->id}/post", $validData);
 
-        $response->assertStatus(200)
-            ->assertJsonCount(4)
-            ->dump();
-    });
+        $response->assertStatus(201)
+            ->assertJson([
+                'content' => $validData['content'],
+            ]);
+    })->with('valid_post_data');
+
+    test('unathenticated user cannot create a post inside the thread', function(array $validData) {
+        $user = User::factory()->create();
+        $thread = Thread::factory()->create();
+        ThreadUser::create([
+            'thread_id' => $thread->id,
+            'user_id' => $user->id,
+            'role_id' => 3,
+        ]);
+
+        $response = $this->postJson("/api/threads/{$thread->id}/post", $validData);
+
+        $response->assertStatus(401);
+    })->with('valid_post_data');
+
+    test('authenticated unauthorized user cannot create a post inside of a thread', function(array $validData){
+        $user = User::factory()->create();
+        $thread = Thread::factory()->create();
+        ThreadUser::create([
+            'thread_id' => $thread->id,
+            'user_id' => $user->id,
+            'role_id' => 4,
+        ]);
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->postJson("/api/threads/{$thread->id}/post", $validData);
+
+        $response->assertStatus(403);
+    })->with('valid_post_data');
+
+    test('trying to create a post inside of a non existent thread should return 404', function(array $validData) {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->postJson("/api/threads/999/post", $validData);
+
+        $response->assertStatus(404);
+    })->with('valid_post_data');
+
+    test('it validates the post data when creating a new post', function (array $invalidData, string $missingField) {
+        $user = User::factory()->create();
+        $thread = Thread::factory()->create();
+        ThreadUser::create([
+            'thread_id' => $thread->id,
+            'user_id' => $user->id,
+            'role_id' => 3,
+        ]);
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->postJson("/api/threads/{$thread->id}/post", $invalidData);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors($missingField);
+    })->with('invalid_post_data');
 });
