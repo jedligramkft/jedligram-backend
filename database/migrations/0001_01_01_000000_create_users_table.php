7b178761899364fce2f410af189283a2ba5a71d7
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2026-03-27 10:07:54 +0100

    refactor migration: increase email field length from 40 to 60 characters

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 40);
-            $table->string('email', 40)->unique();
+            $table->string('email', 60)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
